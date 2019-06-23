fb56c59b7144f8d59ed152caae4e3259eb8f9e41
ibro <kabaltech@gmail.com>
2019-06-23 16:11:26 -0600

    update: JWT done

--- src/helpers/JWTCust.java
+++ src/helpers/JWTCust.java
@@ -1,5 +1,45 @@
 package helpers;
 
+import java.security.Key;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 public class JWTCust {
 
+	public static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+	
+	public static String tokenCreate(String usr) {
+				
+		String jws = Jwts.builder().setSubject(usr).signWith(key).compact();
+				
+		return jws;
+	}
+	
+	public static Boolean tokenRead(String usr) {
+		
+		try 
+		{
+		  Jws<Claims> claims = Jwts.parser().setSigningKey(key).parseClaimsJws(usr);
+		  
+		  String userClaim = claims.getBody().getSubject();
+		
+		  if (userClaim.equals(usr)) {
+			  return true;  
+		  } else {
+			  return false;
+		  }
+		  
+		} catch (JwtException e) 
+		{
+			return false;
+		}
+		
+	}
+	
 }
